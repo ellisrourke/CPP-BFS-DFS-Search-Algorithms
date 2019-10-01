a963310170694a0192daec8a0c7e8e58708cf99b
ellisrourke <ellisrourke@gmail.com>
2019-10-01 11:08:02 +1000

    working implementation

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <sstream>
 #include <iostream>
 #include <stack>
+#include <queue>
 #include <vector>
 #include "gengraph.h"
 class graph{
 public:
     graph(int dimention){
         this->x = dimention;
-        //getdata(x);
+        getdata(x);
         std::cout << "DIMENSION " << x << std::endl;
 
         std::stringstream row;
@@ -30,25 +31,28 @@ public:
     }
 
     void DFS(int startNode,int targetNode){
-        vector<int> parent;
-        vector<int> cost = {99,99,99,99,99};
-        stack<int> stack;
+        vector<int> parent(99, 10);
+        vector<int> cost = {99,99,99,99,99,99,99,99,99,99};
+        //stack<int> stack;
+        queue<int> stack;
         stack.push(startNode);
         cost[startNode] = 0;
 
         while(!stack.empty()){
-            for(int i=0;i<stack.size();i++){
-                std::cout << stack.top() << " ";
-            } std::cout <<" test" << std::endl;
+            std::cout <<"+++ cost array +++" << std::endl;
+            for(int i=0;i<cost.size();i++){
+                std::cout << cost[i] << " ";
+            } std::cout << std::endl << "+++++++++++++++++"<<  std::endl;
 
             int current = stack.top();
             stack.pop();
 
             if (current != targetNode){
                 for(int i =0;i<x;i++){
+                    std::cout << data[i][current] << std::endl;
                     if(data[i][current] > 0){
-                        if(cost[i] > (cost[current] + cost[i])){
-                            cost[i] = (cost[current] + cost[i]);
+                        if(cost[i] > (cost[current] + data[i][current])){
+                            cost[i] = (cost[current] + data[i][current]);
                             parent[i] = current;
                             stack.push(i);
                         }
@@ -56,6 +60,27 @@ public:
                 }
             }
         }
+        //display final
+        std::stack<uint32_t> finalPath;
+        finalPath.push(targetNode);
+        int index = targetNode;
+        while (index != startNode)
+        {
+            finalPath.push(parent[index]);
+            index = parent[index];
+        }
+
+// Print path
+        std::cout << "Optimal Path: ";
+        while (!finalPath.empty())
+        {
+            std::cout << finalPath.top() << " ";
+            finalPath.pop();
+        }
+
+
+
+
     }
 
     void displayRow(){
@@ -70,13 +95,13 @@ public:
 
 private:
     int x;
-    int data[5][5];
+    int data[10][10];
 
 };
 
 int main(){
-    graph g(5);
+    graph g(10);
     g.displayGraph();
-    g.DFS(0,4);
+    g.DFS(0,9);
 
 }
